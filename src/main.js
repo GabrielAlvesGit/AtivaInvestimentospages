9ed12a643dbefad86a1730843ae39aecc6851879
Gabriel Alves <102634725+GabrielAlvesGit@users.noreply.github.com>
2023-10-22 17:49:00 -0300

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-
-// Depois de npm install line-awesome, Importar o css
-import "line-awesome/dist/line-awesome/css/line-awesome.min.css";
-
-createApp(App).mount("#app");
