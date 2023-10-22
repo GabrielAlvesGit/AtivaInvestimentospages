876c388f8c05e9e4753e4a3bc8b673bdfd32f1fb
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2023-10-22 18:06:01 -0300

    hospedagem do projeto vue.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+// Depois de npm install line-awesome, Importar o css
+import "line-awesome/dist/line-awesome/css/line-awesome.min.css";
+
+createApp(App).mount("#app");
